8c3e113f96b2812459c593ce8d4daab144297a88
Rositsa Topchiyska <topchiyska@progress.com>
2018-06-25 19:04:09 +0300

    Android file upload

--- demo/app/activity.android.ts
+++ demo/app/activity.android.ts
@@ -6,8 +6,8 @@ const SELECT_FILE_RESULT_CODE = 100;
 @JavaProxy("org.myApp.MainActivity")
 class Activity extends android.app.Activity {
     private _callbacks: AndroidActivityCallbacks;
-    
-    public uploadCallback: android.webkit.ValueCallback<android.net.Uri>;
+
+    public uploadCallback;
 
     protected onCreate(savedInstanceState: android.os.Bundle): void {
         if (!this._callbacks) {
@@ -18,9 +18,29 @@ class Activity extends android.app.Activity {
         this.uploadCallback = null;
     }
 
-    // public onRequestPermissionsResult(requestCode: number, permissions: Array<String>, grantResults: Array<number>): void {
-    //     this._callbacks.onRequestPermissionsResult(this, requestCode, permissions, grantResults, undefined /*TODO: Enable if needed*/);
-    // }
+    protected onSaveInstanceState(outState: android.os.Bundle): void {
+        this._callbacks.onSaveInstanceState(this, outState, super.onSaveInstanceState);
+    }
+
+    protected onStart(): void {
+        this._callbacks.onStart(this, super.onStart);
+    }
+
+    protected onStop(): void {
+        this._callbacks.onStop(this, super.onStop);
+    }
+
+    protected onDestroy(): void {
+        this._callbacks.onDestroy(this, super.onDestroy);
+    }
+
+    public onBackPressed(): void {
+        this._callbacks.onBackPressed(this, super.onBackPressed);
+    }
+
+    public onRequestPermissionsResult(requestCode: number, permissions: Array<String>, grantResults: Array<number>): void {
+        this._callbacks.onRequestPermissionsResult(this, requestCode, permissions, grantResults, undefined /*TODO: Enable if needed*/);
+    }
 
     protected onActivityResult(requestCode: number, resultCode: number, data: android.content.Intent): void {
         this._callbacks.onActivityResult(this, requestCode, resultCode, data, super.onActivityResult);
@@ -38,7 +58,8 @@ class Activity extends android.app.Activity {
         if (resultCode == android.app.Activity.RESULT_OK) {
             if (data !== null) {
                 if (android.os.Build.VERSION.SDK_INT >= 21) {
-                    uri = android.net.Uri.parse(data.getDataString());
+                    uri = Array.create(android.net.Uri, 1); 
+                    uri[0] = android.net.Uri.parse(data.getDataString());
                 } else {
                     uri = data.getData();
                 }
