5416c42d84684c6c812d9aeff72a1e9700602407
vakrilov <alexander.vakrilov@telerik.com>
2019-03-21 10:25:57 +0200

    chore: update demo app

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,3 +1,4 @@
 ﻿import "./bundle-config";
-import * as application from 'tns-core-modules/application';
-application.start({ moduleName: "main-page" });
+import * as application from "tns-core-modules/application";
+
+application.run({ moduleName: "app-root" });
