55e59d9a90ee3534ba019b9769246c25cf1d38bb
tsvetie <tsvetelina.ivanova@telerik.com>
2019-11-14 17:07:26 +0200

    Make demo page context observable
    That way, you can test whether the change in context will update the chat

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -1,14 +1,17 @@
-import { EventData, fromObject } from 'tns-core-modules/data/observable';
-import { NativeChat } from '@progress-nativechat/nativescript-nativechat';
+import { EventData, fromObject, fromObjectRecursive } from 'tns-core-modules/data/observable';
 import { Page } from 'tns-core-modules/ui/page';
 
 
 export function pageLoaded(args: EventData) {
-    (<Page>args.object).bindingContext = fromObject({
+    (<Page>args.object).bindingContext = fromObjectRecursive({
         nativeChatConfig: {
-            botId: '5acddd9715e7187c15f3fc28',
-            channelId: 'f91f065c-4079-4fa9-8860-b893e2b81696',
-            channelToken: '0570f9a5-6c0e-4b77-b06d-20ce6d5c56d8',
+            bot: {
+                id: '5acddd9715e7187c15f3fc28'
+            },
+            channel: {
+                id: 'f91f065c-4079-4fa9-8860-b893e2b81696',
+                token: '0570f9a5-6c0e-4b77-b06d-20ce6d5c56d8'
+            },
             user: {
                 name: 'John Smith'
             },
